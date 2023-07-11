b8f4c1a872315aaec91319789ea5ddd66eb102bd
yusufva <yusufvalent@gmail.com>
2023-07-12 00:54:14 +0700

    adding refresh point

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var refreshTokenRouter = require('./routes/refreshToken')
 
 var app = express();
 
@@ -20,5 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/token', refreshTokenRouter);
 
 module.exports = app;
---
+++ routes/refreshToken.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const jwt = require('jsonwebtoken');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+router.get('/', async (req,res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        if(!refreshToken) return res.sendStatus(401);
+        const user = await prisma.users.findFirst({ where:{ refresh_token: refreshToken }});
+        if(!user) return res.sendStatus(403);
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decode)=>{
+            if(err) return res.sendStatus(403);
+            const userId = user.id;
+            const name = user.name;
+            const role = user.role;
+            const accessToken = jwt.sign({userId, name, role}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '60s' });
+            res.json({ accessToken });
+        })
+    } catch (e) {
+        res.sendStatus(500);
+    }
+})
+
+module.exports = router;
