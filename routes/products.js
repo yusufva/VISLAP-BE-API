a643616ada9304cdfd5830f1914d20994582da40
yusufva <yusufvalent@gmail.com>
2023-07-19 14:27:26 +0700

    adding product router

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('node:fs/promises')
 const Validator = require('fastest-validator');
 const { Prisma, PrismaClient } = require('@prisma/client')
 const multer = require('multer');
@@ -10,7 +11,7 @@ const productStorage = multer.diskStorage({
         cb(null, './public/images')
     },
     filename: (req,file,cb)=>{
-        cb(null, `produk-${Math.floor(Math.random()*1000)}${Date.now()}`+path.extname(file.originalname))
+        cb(null, `produk-${Math.floor(Math.random()*1000)}${new Date().getTime()}`+path.extname(file.originalname))
     }
 });
 
@@ -47,12 +48,50 @@ router.get('/:id', async (req,res)=> {
     }
 })
 
+router.post('/single', this.upload.single('img'), async (req,res) => {
+    if(!req.file) return res.status(400).json({message:"please upload the image"});
+    const filename = req.file.filename
+    res.json({filename:filename})
+})
+
 router.post('/', this.upload.single('img'), async (req,res)=>{
+    // const schema = {
+    //     nama : 'string|required',
+    //     harga: 'required',
+    //     kategori: 'string|required',
+    //     stock: 'required',
+    // }
+    // const validate = v.validate(req.body, schema);
+    // if (validate.length) {
+    //     return res
+    //     .status(400)
+    //     .json(validate);
+    // }
+
+    if(!req.file) return res.status(400).json({message:"please upload the image"});
+
+    const {nama, harga, kategori, stock} = req.body;
+    const intStock = parseInt(stock);
+    const intHarga = parseInt(harga);
+    const product = await prisma.products.create({
+        data:{
+            nama:nama,
+            harga:intHarga,
+            kategori: kategori,
+            photo: req.file.filename,
+            stock: intStock,
+        }
+    })
+
+    res.json(product)
+})
+
+router.put('/:id', this.upload.single('img'), async (req,res)=>{
     const schema = {
-        nama : 'string',
-        harga: 'number|required',
-        kategori: 'string',
-        stock: 'number',
+        nama : 'string|required',
+        harga: 'required',
+        kategori: 'string|required',
+        stock: 'required',
     }
     const validate = v.validate(req.body, schema);
     if (validate.length) {
@@ -61,24 +100,54 @@ router.post('/', this.upload.single('img'), async (req,res)=>{
         .json(validate);
     }
 
-    const {nama, harga, kategori, stock} = req.body;
-    // const intStock = parseInt(stock);
-    // const intHarga = parseInt(harga);
-    const product = await prisma.products.create({
-        data:{
-            nama:nama,
-            harga:harga,
-            kategori: kategori,
-            photo: req.file.filename,
-            stock: stock,
+    if(!req.file) {
+        try {
+            const id = parseInt(req.params.id);
+            
+            let product = await prisma.products.findUnique({where:{id:id}})
+            if (!product) return res.status(404).json({message:"product not found"})
+
+            const {nama, harga, kategori, stock} = req.body;
+            const intStock = parseInt(stock);
+            const intHarga = parseInt(harga);
+            product = await prisma.products.update({
+                data:{
+                    nama:nama,
+                    harga:intHarga,
+                    kategori: kategori,
+                    stock: intStock,
+                }
+            })
+        
+            return res.json(product)
+        } catch (e) {
+            return res.status(400).json({message:"id must be a number"})
         }
-    })
-
-    res.json(product)
-})
-
-router.put('/:id', async (req,res)=>{
-
+    } else {
+        try {
+            const id = parseInt(req.params.id);
+            
+            let product = await prisma.products.findUnique({where:{id:id}})
+            if (!product) return res.status(404).json({message:"product not found"})
+    
+            const {nama, harga, kategori, stock} = req.body;
+            const intStock = parseInt(stock);
+            const intHarga = parseInt(harga);
+            product = await prisma.products.update({
+                data:{
+                    nama:nama,
+                    harga:intHarga,
+                    kategori: kategori,
+                    photo: req.file.filename,
+                    stock: intStock,
+                }
+            })
+        
+            return res.json(product)
+        } catch (e) {
+            return res.status(400).json({message:"id must be a number"})
+        }
+    }
 })
 
 router.delete('/:id', async (req,res)=>{
@@ -86,8 +155,12 @@ router.delete('/:id', async (req,res)=>{
     try {
         const id = parseInt(param);
         let product = await prisma.products.findUnique({where:{id:id}});
+
         if(!product) return res.status(404).json({message:"product not fount"});
+
+        fs.unlink(`./public/images/${product.photo}`)
         product = await prisma.products.delete({where:{id:id}})
+
         return res.json({message:"product successfully deleted"})
     } catch (e) {
         return res.status(400).json({message:"id must be a number"})
