ecf606573db0cd02fda5dc42402148b11ea7539a
yusufva <yusufvalent@gmail.com>
2023-07-19 14:27:47 +0700

    adding transaction router

--- routes/transaction.js
+++ routes/transaction.js
@@ -7,7 +7,15 @@ const prisma = new PrismaClient()
 const v = new Validator();
 
 router.get('/', async (req,res) => {
-    const tx = await prisma.transactions.findMany({include:{items:true}})
+    const tx = await prisma.transactions.findMany({
+        include:{
+            items:true,
+            status:{
+                select:{
+                    status:true
+                }
+            }
+        }})
     res.json(tx)
 })
 
@@ -17,7 +25,7 @@ router.get('/:id', async (req,res) => {
         const tx = await prisma.transactions.findUnique({
             where: {id:id},
             include:{
-                items: true
+                items: true,
             }
         });
     
@@ -51,12 +59,41 @@ router.post('/', async (req,res) => {
             user_id: req.body.user_id,
             final_price: req.body.final_price,
             items:{
-                createMany: req.body.items
-            }
+                createMany: {
+                    data: req.body.items
+                }
+            },
+            expiration: new Date(Date.now() + 24 * 60 * 60 * 1000)
+        },
+        include:{
+            items:true
         }
     });
 
     res.status(201).json(tx);
+})
+
+router.put('/:id', async (req,res) => {
+    try {
+        const id = parseInt(req.params.id);
+        let tx = await prisma.transactions.findUnique({where:{id:id}})
+        if (!tx) return res.status(404).json({message:"transaction not found"})
+
+        const schema = {
+            status: "number|required"
+        }
+        const validate = v.validate(req.body, schema);
+        if (validate.length) {
+            return res
+            .status(400)
+            .json(validate);
+        };
+
+        tx = await prisma.transactions.update({where:{id:id}, data:req.body, include:{items:true}})
+        return res.json(tx)
+    } catch (e) {
+        
+    }
 })
 
 router.delete('/:id', async (req,res) => {
