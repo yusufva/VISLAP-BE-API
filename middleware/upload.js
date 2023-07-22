1ebfa48dfb587e01a4c987dfe87a61a284ad700f
yusufva <yusufvalent@gmail.com>
2023-07-22 13:19:42 +0700

    feat(middleware)!: upload image middleware

---
+++ middleware/upload.js
@@ -0,0 +1,24 @@
+
+const multer = require('multer');
+const path = require('path')
+
+const productStorage = multer.diskStorage({
+    destination: (req,file,cb)=> {
+        cb(null, './public/images')
+    },
+    filename: (req,file,cb)=>{
+        cb(null, `produk-${Math.floor(Math.random()*1000)}${new Date().getTime()}`+path.extname(file.originalname))
+    }
+});
+
+const productFilter = (req, file, cb) => {
+    if (!file.originalname.match(/\.(png|jpg)$/)){
+        return cb(new Error('Please upload a Image'))
+    }
+    cb(null, true)
+};
+
+exports.upload = multer({
+    storage: productStorage,
+    fileFilter: productFilter
+});
