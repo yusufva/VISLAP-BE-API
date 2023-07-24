b89169893611be51323bca66a8c449ab31d7b767
yusufva <yusufvalent@gmail.com>
2023-07-24 14:50:18 +0700

    feat: adding jwt auth for user id

--- middleware/jwtauth.js
+++ middleware/jwtauth.js
@@ -25,8 +25,24 @@ const auth = (roles) => async(req,res,next) => {
             next();
         })
     } catch (e) {
-        res.status(401).json({message:"catch"});
+        res.status(500).json({message:"something went wrong"});
     }
 }
 
-module.exports = {auth, verifyToken}
+const authId = (id) => async(req,res,next) => {
+    try {
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1];
+        if(token == null) return res.status(401).json({message:"no token"});
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decode)=>{
+            if(err) return res.sendStatus(403);
+            if(!id.includes(decode.userId)) return res.status(401).json({message:"Unauthorized"})
+            req.user = decode;
+            next();
+        })
+    } catch (e) {
+        res.status(500).json({message:"something went wrong"});
+    }
+}
+
+module.exports = {auth, verifyToken, authId}
